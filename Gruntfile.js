5f831f476a59a3f6d0192d6e8e84f3f99ef47139
Virgil Ierubino <webmaster@convincible.media>
2018-05-10 17:37:29 +0100

    Asset concatenation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,136 +1,165 @@
 'use strict';
 module.exports = function(grunt) {
 
-   grunt.loadNpmTasks('grunt-shell');
-   grunt.loadNpmTasks('grunt-concurrent');
-   grunt.loadNpmTasks('grunt-sass');
-   grunt.loadNpmTasks('grunt-postcss');
-   grunt.loadNpmTasks('grunt-contrib-htmlmin');
-   grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-      shell: {
-         jekyllBuild: {
-            command: 'jekyll build'
-         },
-         jekyllServe: {
-            command: 'jekyll serve --no-watch --skip-initial-build'
-         },
-         datoDump: {
-            command: 'bundle exec dato dump'
-         },
-         datoBackup: {
-            command: 'node dato.backup.js'
-         }
-      },
+		dirs: {
+			src: '/source',
+			dest: '/public',
+		},
 
-      concurrent: {
-         optimise: [
-            'postcss:minify', 'htmlmin', 'uglify' // Output files are optimised
-         ]
-      },
+		shell: {
+			jekyllBuild: {
+				command: 'jekyll build'
+			},
+			jekyllServe: {
+				command: 'jekyll serve --no-watch --skip-initial-build'
+			},
+			datoDump: {
+				command: 'bundle exec dato dump'
+			},
+			datoBackup: {
+				command: 'node dato.backup.js'
+			}
+		},
 
-      // =============
-      // BUILD PROCESS
-      // =============
+		concurrent: {
+			optimise: [
+				'postcss:minify', 'htmlmin', 'uglify' // Output files are optimised
+			]
+		},
 
-      sass: {
-         options: {
-            sourceMap: true,
-            relativeAssets: false,
-            outputStyle: 'expanded',
-            sassDir: 'source/assets/css',
-            cssDir: 'public/assets/css'
-         },
-         build: {
-            files: [{
-               expand: true,
-               cwd: 'source/assets/css/',
-               src: ['**/*.scss'],
-               dest: 'public/assets/css/',
-               ext: '.css'
-            }]
-         }
-      },
+		// =============
+		// CONCATENATION
+		// =============
 
-      // ===============
-      // POST-PROCESSING
-      // ===============
+		concat: {
+			prod: { // Target
+				options: {
+				//	//banner: "'use strict';\n",
+				//	process: function(src, filepath) {
+				//		return src;
+				//	},
+				//	separator: "\n"
+				},
+				files: { // destination: [sources]
+					'<%= dirs.dest %>/assets/js/main.js': [
+						'<%= dirs.src %>/assets/js/helpers/*.js',
+						'<%= dirs.src %>/assets/js/layouts/page.js',
+					],
+				},
+			},
+		},
 
-      postcss: {
-         options: {
-            map: true
-         },
-         autoprefix: {
-            options: {
-               processors: [
-                  require('autoprefixer')({
-                     remove: false,
-                     grid: true
-                  })
-               ]
-            },
-            src: 'public/assets/css/**/*.css'
-         },
-         minify: {
-            options: {
-               processors: [
-                  require('cssnano')()
-               ]
-            },
-            src: 'public/assets/css/**/*.css'
-         }
-      },
+		// =============
+		// COMPILATION
+		// =============
 
-      htmlmin: {
-         prod: { // Target
-            options: {
-               removeComments: true,
-               collapseWhitespace: true
-            },
-            files: [{
-               expand: true,
-               cwd: 'public/',
-               src: ['**/*.html'],
-               dest: 'public/'
-            }]
-         }
-      },
+		sass: {
+			options: {
+				sourceMap: true,
+				relativeAssets: false,
+				outputStyle: 'expanded',
+				sassDir: 'source/assets/css',
+				cssDir: 'public/assets/css'
+			},
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'source/assets/css/',
+					src: ['**/*.scss'],
+					dest: 'public/assets/css/',
+					ext: '.css'
+				}]
+			}
+		},
 
-      uglify: {
-         prod: {
-            options: {
-               compress: true,
-               mangle: false,
-               sourceMap: true
-            },
-            files: [{
-               expand: true,
-               cwd: 'public/assets/js/',
-               src: ['**/*.js', '!**/*.min.js'],
-               dest: 'public/assets/js/'
-            }]
-         }
-      }
+		// ===============
+		// POST-PROCESSING
+		// ===============
 
-   });
+		postcss: {
+			options: {
+				map: true
+			},
+			autoprefix: {
+				options: {
+					processors: [
+						require('autoprefixer')({
+							remove: false,
+							grid: true
+						})
+					]
+				},
+				src: 'public/assets/css/**/*.css'
+			},
+			minify: {
+				options: {
+					processors: [
+						require('cssnano')()
+					]
+				},
+				src: 'public/assets/css/**/*.css'
+			}
+		},
 
-   grunt.registerTask('build', [
-      'shell:jekyllBuild', // Jekyll builds markdown/liquid
-      'sass', 'postcss:autoprefix'
-   ]);
-   grunt.registerTask('optimise', [
-      'concurrent:optimise'
-   ]);
-   grunt.registerTask('serve', [
-      'shell:jekyllServe'
-   ]);
-   grunt.registerTask('backup', [
-      'shell:datoBackup'
-   ]);
+		htmlmin: {
+			prod: { // Target
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'public/',
+					src: ['**/*.html'],
+					dest: 'public/'
+				}]
+			}
+		},
 
-   grunt.registerTask('default', 'build');
+		uglify: {
+			prod: {
+				options: {
+					compress: true,
+					mangle: false,
+					sourceMap: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'public/assets/js/',
+					src: ['**/*.js', '!**/*.min.js'],
+					dest: 'public/assets/js/'
+				}]
+			}
+		}
+
+	});
+
+	grunt.registerTask('build', [
+		'shell:jekyllBuild', // Jekyll builds markdown/liquid
+		// Concatenate files
+		'sass', // Compile higher-order languages
+		'postcss:autoprefix'
+	]);
+	grunt.registerTask('optimise', [
+		'concurrent:optimise'
+	]);
+	grunt.registerTask('serve', [
+		'shell:jekyllServe'
+	]);
+	grunt.registerTask('backup', [
+		'shell:datoBackup'
+	]);
+
+	grunt.registerTask('default', 'build');
 
 };
