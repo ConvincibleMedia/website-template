fa5388caf505cd2c0b488734db131c7ec8e60837
Virgil Ierubino <webmaster@convincible.media>
2018-06-04 15:58:04 +0100

    Form adjustments

--- source/_plugins/tags/table.rb
+++ source/_plugins/tags/table.rb
@@ -1,20 +1,15 @@
-class TableTag < Liquid::Block
+module Jekyll
+	class TableBlock < CustomBlock
+		def output
 
-	def initialize(tag_name, markup, tokens)
-		super
-		# tag_name = this tag's own name
-		# markup = string passed with opening tag
-		# tokens = options from Liquid
-		@class = markup
+			table = HTML::element('div')
+			table['class'] = (@input ? 'table-' + @input : 'table')
+			table.inner_html = @markdown.convert(@block)
+
+			return table.to_html
+
+		end
 	end
-
-	def render(context)
-		# calling `super` returns the content of the block
-		table = context.registers[:site].find_converter_instance(Jekyll::Converters::Markdown).convert(super.to_s)
-		return '<div class="table' + (@class ? ' table-' + @class : '') + '">' + table + '</div>'
-
-	end
-
 end
 
-Liquid::Template.register_tag('table', TableTag)
+Liquid::Template.register_tag('table', Jekyll::TableBlock)
